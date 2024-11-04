18accf63c10cdfb537d27836b426efe558aaaf05
Edward <edward.bock@palasthotel.de>
2024-11-04 21:58:13 +0100

    logs expire after 30 days by default

--- plugin/classes/Log.php
+++ plugin/classes/Log.php
@@ -120,7 +120,7 @@ class Log  extends Database {
 
 	function clean(): void {
 		$table     = $this->table;
-		$days      = apply_filters( Plugin::FILTER_EXPIRE, 14 );
+		$days      = apply_filters( Plugin::FILTER_EXPIRE, 30 );
 		$parentIds = "SELECT id FROM (" .
 		             "SELECT id FROM " . $this->table . " WHERE " .
 		             "parent_id IS NULL AND " .
