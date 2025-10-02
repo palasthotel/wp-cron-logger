8d486d57b6d8e09bc4e591e977e14e8a9749fc48
Lucas Regalar <lucas.regalar@palasthotel.de>
2025-10-02 11:00:09 +0200

    composer i

---
+++ plugin/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'palasthotel/cron-logger',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '361bd69f893444c2041f806ffb62a1dc7d66b473',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'palasthotel/cron-logger' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '361bd69f893444c2041f806ffb62a1dc7d66b473',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
